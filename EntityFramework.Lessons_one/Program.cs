c36bfb6bf773d55d4e7ba4278acf32e74694af2d
yojik2901 <yojik2901@gmail.com>
2019-03-17 21:51:25 +0300

    #1 - Соединение с базой данных и вывод данных из одной таблицы

--- EntityFramework.Lessons_one/Program.cs
+++ EntityFramework.Lessons_one/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntityFramework.Lessons_one.Properties;
 
 namespace EntityFramework.Lessons_one
 {
@@ -10,6 +13,24 @@ namespace EntityFramework.Lessons_one
     {
         static void Main(string[] args)
         {
+            using (IDbConnection connection = new SqlConnection(Settings.Default.DbConnect))
+            {
+                IDbCommand command = new SqlCommand("SELECT * FROM t_customer");
+                command.Connection = connection;
+
+                connection.Open();
+
+                IDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine("Идентификатор: {0}\t Имя: {1}",
+                        reader.GetInt32(0),
+                        reader.GetString(1));
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
